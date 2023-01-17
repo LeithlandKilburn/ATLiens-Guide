bc6e85d1187328ca213bc67f37c827c8c2234221
LeithlandKilburn <kilburnleithland@gmail.com>
2023-01-17 11:22:01 -0500

    Building out user tests

--- server/src/main/java/learn/atliens/controller/UserController.java
+++ server/src/main/java/learn/atliens/controller/UserController.java
@@ -1,4 +1,20 @@
 package learn.atliens.controller;
+import learn.atliens.model.User;
+import learn.atliens.repo.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/atliens")
 
 public class UserController {
+    @Autowired
+    private UserRepo repo;
+
+    @GetMapping
+    public List<User> findAll(){
+        return repo.findAll();
+    }
 }
