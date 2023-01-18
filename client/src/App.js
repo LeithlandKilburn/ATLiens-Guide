2196c98d3806cecedc882ea2ae4b3dc118048fc0
LeithlandKilburn <kilburnleithland@gmail.com>
2023-01-18 10:26:08 -0500

    Finished add user methods in backend and started login flow on front end

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,15 @@
 import {useState, useEffect} from "react"; 
 import { Routes, Route, Link, Router } from 'react-router-dom';
 import Home from './components/Home.js';
+import Login from './components/Login.js';
 
 
 function App() {
   return (
     <div className="App">
-    <h1>333</h1>
       <Routes>
           <Route path="/" element={<Home/>}/>
+          <Route path="/login" element={<Login/>}/>
       </Routes>
     </div>
   );
