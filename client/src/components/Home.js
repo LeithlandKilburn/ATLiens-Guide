ed0b9b8a4fb90ea5e195a1893ad2337d264244fa
LeithlandKilburn <kilburnleithland@gmail.com>
2023-01-23 14:14:36 -0500

    starting featured words component

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -2,10 +2,16 @@ import React from 'react';
 import { useContext } from 'react';
 
 import SearchBar from './SearchBar';
+import FeaturedWords from './FeaturedWords';
 import AuthContext from '../contexts/AuthContext';
 
 function Home({ wordData, setWordData }) {
-  return <SearchBar wordData={wordData} setWordData={setWordData} />;
+  return (
+    <div>
+      <SearchBar wordData={wordData} setWordData={setWordData} />
+      <FeaturedWords wordData={wordData}/>
+    </div>
+  )
 }
 
 export default Home;
